3e2151d02cb5a76f105fa336be6a3240e0821de0
Tom Koltunov <tomkoltunov@gmail.com>
2025-12-29 01:42:24 +0200

    fix HomePage - still need to check and correct the filtering function

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -3,6 +3,15 @@ import { useProducts } from "../../hooks/useProducts";
 import { useFilteredProducts } from "../../hooks/useFilteredProducts";
 import { FiltersDropdown } from "./FiltersDropdown";
 import { ProductList } from "./components/ProductsList";
+import type { Product } from "../../types/productType";
+
+export const stringToDateConverter = (stringFormatDate: string): Date => {
+  const extractedYear = Number(stringFormatDate.substring(0, 4));
+  const extractedMonth = Number(stringFormatDate.substring(5, 7));
+  const extractedDay = Number(stringFormatDate.substring(8, 10));
+  const result = new Date(extractedYear, extractedMonth, extractedDay);
+  return result;
+};
 
 export const HomePage = () => {
   const { products, isLoading } = useProducts({ delay: 500 });
@@ -22,7 +31,34 @@ export const HomePage = () => {
     return [...new Set(categories)];
   }, [products]);
 
-  console.log(minPrice);
+  const filteredProducts = useMemo((): Product[] => {
+    return products
+      .filter((product) =>
+        product.name.toLowerCase().includes(searchValue) ||
+        product.description.toLowerCase().includes(searchValue)
+          ? product
+          : ""
+      )
+      .filter((product) =>
+        uniqueCategories.includes(product.category) ? "" : product
+      )
+      .filter((product) =>
+        product.price >= minPrice && product.price <= maxPrice ? product : ""
+      )
+      .filter((product) =>
+        stringToDateConverter(product.uploadingDate) <=
+        stringToDateConverter(uploadingDate)
+          ? ""
+          : product
+      );
+  }, [
+    products,
+    searchValue,
+    uniqueCategories,
+    minPrice,
+    maxPrice,
+    uploadingDate,
+  ]);
 
   return (
     <div
@@ -48,7 +84,7 @@ export const HomePage = () => {
         uploadingDate={uploadingDate}
         setUploadingDate={setUploadingDate}
       />
-      <ProductList products={products} />
+      <ProductList products={filteredProducts} />
     </div>
   );
 };
--- src/types/filteredProductsContextType.ts
+++ src/types/filteredProductsContextType.ts
@@ -1,20 +1,3 @@
-import type { Product } from "./productType";
-
-// export type FilteredProductsContextType = {
-//   filteredProducts: Product[];
-//   setFilteredProducts: (filteredProducts: Product[]) => void;
-//   filterProductsByCategory: (category: string) => void;
-//   filterProductsByPrice: (price: number, priceFilterBy: "min" | "max") => void;
-//   filterProductsByUploadingDate: (uploadingDate: string) => void;
-// };
-
-export type FiltersState = {
-  filtersCategories: string[];
-  filtersMinPrice: number;
-  filtersMaxPrice: number;
-  filtersUploadingDate: string;
-};
-
 export type FiltersStateContextType = {
   categories: string[];
   setCategories: (categories: string[]) => void;
